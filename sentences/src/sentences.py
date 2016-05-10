4654b8aaa9e289a60ea9a0b83d3944fcc4168f7b
pstanisl <petr.stanislav@gmail.com>
2016-05-10 15:45:04 +0200

    Filter sentences with on the vocab pairs.

---
+++ sentences/src/sentences.py
@@ -0,0 +1,116 @@
+import argparse
+
+import codecs
+
+from tools import fn_timer
+
+try:
+    from progressbar import ProgressBar
+    SHOW_PROGRESS = True
+except:
+    SHOW_PROGRESS = False
+
+# Define script input arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-o', '--output', action='store',
+                    help='path to the file with combined vocabs and sentences')
+parser.add_argument('-s', '--sentences', action='store',
+                    help='path to the file with sentences')
+parser.add_argument('-v', '--vocabs', action='store',
+                    help='path to the file with vocabs')
+
+parser.set_defaults(output='./output.txt')
+
+
+def get_sentences(path, *, encoding='utf-8'):
+    """Load sentences from file. Every sentence on a single file.
+
+    Args:
+        path: path to the file with sentences,
+        encoding: encoding of the content in the file (default=utf-8)
+
+    Yields:
+        sentence: string with loaded sentence
+    """
+    with codecs.open(path, encoding=encoding) as sentences:
+        for sentence in sentences.readlines():
+            # Remove white space from the beginning and the end.
+            sentence = sentence.strip()
+            # Skip empty sentence.
+            if not sentence:
+                continue
+            # Return loaded sentence.
+            yield sentence
+
+
+def get_vocabs(path, *, encoding='utf-8'):
+    """Load vocabs from file. Every vocab pair on a single file.
+
+    Args:
+        path: path to the file with sentences,
+        encoding: encoding of the content in the file (default=utf-8)
+
+    Yields:
+        vocav: string with loaded vocabs pair
+    """
+    with codecs.open(path, encoding=encoding) as vocabs:
+        for vocab in vocabs.readlines():
+            # Remove white space from the beginning and the end.
+            vocab = vocab.strip()
+            # Skip empty line.
+            if not vocab:
+                continue
+            # Return loaded vocabs pair.
+            yield vocab
+
+
+def find_sentences(vocabs, sentences):
+    for vocab in vocabs:
+        vocab1, vocab2 = vocab.lower().split(' - ')
+        # Find all sentences with the vocab. It is not optimal but
+        # performance is not bad at all.
+        filtered1 = filter(
+            lambda sentence: vocab1 in sentence.lower().split(), sentences)
+        filtered2 = filter(
+            lambda sentence: vocab2 in sentence.lower().split(), sentences)
+        # Return vocab and all related sentences.
+        yield vocab, filtered1, filtered2
+
+
+def save_senteces(path, combined, *, encoding='utf-8'):
+    with codecs.open(path, 'w', encoding=encoding) as output:
+        for vocab, sentences1, sentences2 in combined:
+            sentences1 = list(sentences1)
+            sentences2 = list(sentences2)
+            # Skip if one of the list is empty. Imposible to find sentences
+            # for a vocab.
+            if len(sentences1) == 0 or len(sentences2) == 0:
+                continue
+            # Save into a file.
+            output.write('## {}\n'.format(vocab))
+            # Save sentences for the first vocab.
+            output.write('# - 1\n')
+            for sentence in sentences1:
+                output.write('{}\n'.format(sentence))
+            # Save sentences for the second vocab.
+            output.write('# - 2\n')
+            for sentence in sentences2:
+                output.write('{}\n'.format(sentence))
+
+
+@fn_timer
+def main(args):
+    sentences = list(get_sentences(args.sentences))
+    vocabs = get_vocabs(args.vocabs)
+
+    combined = find_sentences(vocabs, sentences)
+
+    save_senteces(args.output, combined)
+
+if __name__ == '__main__':
+    from sys import exit
+
+    args = parser.parse_args()
+
+    exit(int(main(args) or 0))
---
+++ sentences/src/tools.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+
+import time
+
+from functools import wraps
+from numba import jit
+
+
+# @jit
+@jit(nogil=True)
+# @jit(nopython=True, cache=True)
+def get_index(l, x):
+    """Get index of an item `x` in the list `l`. If the
+    item is not in the list return `-1`.
+
+    Examples:
+        >>> l = [1, 2, 3, 4]
+        >>> get_index(l, 1)
+        0
+        >>> get_index(l, 5)
+        -1
+    """
+    return l.index(x) if x in l else -1
+
+
+def fn_timer(function):
+    @wraps(function)
+    def function_timer(*args, **kwargs):
+        t0 = time.time()
+        result = function(*args, **kwargs)
+        t1 = time.time()
+        print('Total time running {}: {:.4f} seconds'.format(
+            function.__name__, t1 - t0))
+        return result
+    return function_timer
