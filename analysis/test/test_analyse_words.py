0e10b3165af97e4bfee84e0aea8df5b92647373e
pstanisl <petr.stanislav@gmail.com>
2016-06-03 16:08:59 +0200

    Better test case for diffece on the begining - FAILING

--- analysis/test/test_analyse_words.py
+++ analysis/test/test_analyse_words.py
@@ -1,10 +1,10 @@
 import unittest
 
-import analyse_words
+import analyse
 from lcs import lcs_mat
 
 
-class AnalyseWordsTetstCase(unittest.TestCase):
+class AnalyseTetstCase(unittest.TestCase):
 
     def test_get_diffs(self):
         # ex1 - 'ABCD' vs. 'ABED'
@@ -13,7 +13,7 @@ class AnalyseWordsTetstCase(unittest.TestCase):
         # Get LCS matrix.
         matrix = lcs_mat(ref, rec)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, ref, rec))
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
         # Compare
         self.assertEqual([(['C'], ['E'])], diffs)
         # ex2 - 'A B C D' vs. 'A B C1 C2 D'
@@ -22,14 +22,14 @@ class AnalyseWordsTetstCase(unittest.TestCase):
         # Get LCS matrix.
         matrix = lcs_mat(ref, rec)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, ref, rec))
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
         # Compare
         self.assertEqual([(['C'], ['C1', 'C2'])], diffs)
         # ex3 - 'A B C1 C2 D' vs. 'A B C D'
         # Get LCS matrix.
         matrix = lcs_mat(rec, ref)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, rec, ref))
+        diffs = list(analyse.get_diffs(matrix, rec, ref))
         # expected = [(['C1', 'C2'], ['C'])]
         # Compare
         self.assertEqual([(['C1', 'C2'], ['C'])], diffs)
@@ -39,14 +39,14 @@ class AnalyseWordsTetstCase(unittest.TestCase):
         # Get LCS matrix.
         matrix = lcs_mat(ref, rec)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, ref, rec))
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
         # Compare
         self.assertEqual([(['C'], [])], diffs)
         # ex5 - 'A B D' vs. 'A B C D'
         # Get LCS matrix.
         matrix = lcs_mat(rec, ref)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, rec, ref))
+        diffs = list(analyse.get_diffs(matrix, rec, ref))
         # Compare
         self.assertEqual([([], ['C'])], diffs)
         # ex5 - 'A B C D E F' vs. 'A B C1 C2 D G F'
@@ -55,14 +55,14 @@ class AnalyseWordsTetstCase(unittest.TestCase):
         # Get LCS matrix.
         matrix = lcs_mat(ref, rec)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, ref, rec))
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
         # Compare
         self.assertEqual([(['E'], ['G']), (['C'], ['C1', 'C2'])], diffs)
         # ex6 - 'A B C1 C2 D G F' vs. 'A B C D E F'
         # Get LCS matrix.
         matrix = lcs_mat(rec, ref)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, rec, ref))
+        diffs = list(analyse.get_diffs(matrix, rec, ref))
         # Compare
         self.assertEqual([(['G'], ['E']), (['C1', 'C2'], ['C'])], diffs)
         # ex7 - 'ABCD' vs. 'FBCD'
@@ -71,9 +71,18 @@ class AnalyseWordsTetstCase(unittest.TestCase):
         # Get LCS matrix.
         matrix = lcs_mat(ref, rec)
         # Find all differences.
-        diffs = list(analyse_words.get_diffs(matrix, ref, rec))
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
         # Compare
         self.assertEqual([(['A'], ['F'])], diffs)
+        # ex8 - 'AGDBCD' vs. 'FBCD'
+        ref = 'AGDBCD'
+        rec = 'FBCD'
+        # Get LCS matrix.
+        matrix = lcs_mat(ref, rec)
+        # Find all differences.
+        diffs = list(analyse.get_diffs(matrix, ref, rec))
+        # Compare
+        self.assertEqual([(['A', 'G', 'D'], ['F'])], diffs)
 
 
 if __name__ == '__main__':
