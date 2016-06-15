a3b2e95e5186d0de3330b2b06c8fddf0116d46ef
pstanisl <petr.stanislav@gmail.com>
2016-06-15 14:29:03 +0200

    Generic save method

--- tools/src/utils.py
+++ tools/src/utils.py
@@ -1,5 +1,6 @@
 import codecs
 
+
 def load_file(path, *, encoding='utf-8'):
     """Load raw text content of the file.
 
@@ -19,3 +20,17 @@ def load_file(path, *, encoding='utf-8'):
             if not line:
                 continue
             yield line
+
+
+def save_file(path, data, *, encoding='uft-8'):
+    """Save items from an iterator into a file. On the end of the
+    file is empty line.
+
+    Args:
+        path: path to the output file
+        data: an iterator with content
+        encoding: encoding of the output file
+    """
+    with codecs.open(path, 'w', encoding=encoding) as target:
+        for item in data:
+            target.write('{}\n'.format(item))
