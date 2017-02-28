d455d8dbc7691e73291f15c27d4589f60933d17f
pstanisl <petr.stanislav@gmail.com>
2017-02-28 13:39:12 +0100

    testing creating new pairs

---
+++ tools/test/test_create_pairs.py
@@ -0,0 +1,30 @@
+import unittest
+
+import create_pairs
+
+
+class CratePairsTestCase(unittest.TestCase):
+
+    def test_get_pairs(self):
+        pairs = [
+            ('A', 'B', '1'),
+            ('D', 'E', '2'),
+            ('G', 'F', '3'),
+            ('B', 'A', '4'),
+            ('F', 'G', '5'),
+            ('E', 'D', '6')
+        ]
+
+        expected = [
+            'A;B;1;4',
+            'D;E;2;6',
+            'G;F;3;5'
+        ]
+
+        reduced = list(create_pairs.get_pairs(pairs))
+
+        self.assertEqual(expected, reduced)
+
+
+if __name__ == '__main__':
+    unittest.main()
