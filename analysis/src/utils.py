80fa43d9d5fe3300cdbb27e07459f0f5ddb8bb5e
pstanisl <petr.stanislav@gmail.com>
2017-03-02 10:48:08 +0100

    Save file function

--- analysis/src/utils.py
+++ analysis/src/utils.py
@@ -127,3 +127,17 @@ def load_mlf_to_dict(path, *, encoding='utf-8'):
         ret[key] += content
 
     return ret
+
+
+def save_file(path, data, *, encoding='uft-8'):
+    """Save items from an iterator into a file. On the end of the
+    file is empty line.
+
+    Args:
+        path: path to the output file
+        data: an iterator with content
+        encoding: encoding of the output file
+    """
+    with codecs.open(path, 'w', encoding=encoding) as target:
+        for item in data:
+            target.write('{}\n'.format(item))
