b522a5435d81e191185e722bb5cb9dcebd179bfa
pstanisl <petr.stanislav@gmail.com>
2018-07-11 16:09:17 +0200

    Updated path to the source files

--- analysis/tests/__init__.py
+++ analysis/tests/__init__.py
@@ -1,6 +1,6 @@
 from os.path import abspath, dirname, join
 import sys
 # Get SRC directory path.
-SRC_DIR = abspath(join(dirname(__file__), '..', 'src'))
+SRC_DIR = abspath(join(dirname(__file__), '..'))
 # Add src dir into PATHs.
 sys.path.append(SRC_DIR)
